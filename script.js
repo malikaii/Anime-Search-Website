2793f64238952b5129b433ac3a8221146de1f10b
malikaii <70463027+malikaii@users.noreply.github.com>
2021-02-14 00:48:35 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let baseUrl = "https://api.jikan.moe/v3";
+
+
+
+
+document.getElementById('button').addEventListener('click', (e) =>{
+    e.preventDefault();
+    const input = document.getElementById('searchbar').value;
+    $.getJSON(`${baseUrl}/search/anime?q=${input}&page=1`, function(data){
+
+    console.log(data);
+    
+    var image = data.results[0].image_url;
+    var title = data.results[0].title;
+    var episodes = data.results[0].episodes;
+
+    //$('.airing').append(isAiring);
+
+    $('.icon').attr('src', image);
+    $('.title').append(title);
+    $('.episodes').append("This show has " + episodes + " episodes");            
+ 
+
+});
+
+    
+
+})
+
+
+
+
