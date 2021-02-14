35f4c3b2f8664ceb043f82e91917313700368c81
malikaii <70463027+malikaii@users.noreply.github.com>
2021-02-14 02:41:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-
 let baseUrl = "https://api.jikan.moe/v3";
-
-
 
 
 document.getElementById('button').addEventListener('click', (e) =>{
@@ -14,19 +11,26 @@ document.getElementById('button').addEventListener('click', (e) =>{
     var image = data.results[0].image_url;
     var title = data.results[0].title;
     var episodes = data.results[0].episodes;
+    var isAiring = data.results[0].airing;
+    if(isAiring == false)
+    {
+        $('.airing').append("This show is no longer running");
+    }
+    else if(isAiring == true)
+    {
+        $('.airing').append("This show is currently running");
 
-    //$('.airing').append(isAiring);
+    }
+    
+    
+    
 
     $('.icon').attr('src', image);
     $('.title').append(title);
     $('.episodes').append("This show has " + episodes + " episodes");            
- 
-
-});
-
     
 
-})
+});
 
 
 
